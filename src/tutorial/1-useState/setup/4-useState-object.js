7e72e5d914127645845f7966eea3c2337bb5815c
Harshit Singh <harshit15hs@gmail.com>
2021-08-10 11:53:10 +1000

    UseSate - Object Example
    Apart from using one large object we can set up multiple small state values.

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // const [person, setPerson] = useState({
+  //   name:'peter',
+  //   age:24,
+  //   message:'random message'
+  // });
+
+  const [name, setName] = useState('Peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('random message world')
+
+const changeMessage = () => {
+  // setPerson({...person, message:'hello world'});
+  setMessage('hello world');
+};
+
+  return (
+    <>
+      {/* <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3> */}
+
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
